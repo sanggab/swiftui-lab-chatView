21ffc37be974296e374a214ac332f40cfcd2301b
심상갑 <kappa2410@gmail.com>
2024-07-15 16:33:28 +0900

    Initial Commit

---
+++ swiftui-lab-chatView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui-lab-chatView
+//
+//  Created by Gab on 2024/07/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui-lab-chatView/swiftui_lab_chatViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_lab_chatViewApp.swift
+//  swiftui-lab-chatView
+//
+//  Created by Gab on 2024/07/15.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_lab_chatViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
